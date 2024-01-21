c32ea9a2cb3bff26fb261b7a655e3cb2c3f2944d
Sakiyary <sakiyary19@qq.com>
2024-01-22 00:19:42 +0800

    change json format

--- src/main.c
+++ src/main.c
@@ -38,7 +38,7 @@ int main() {
           .isRinshan = false,
       },
   };
-  Result const *result = majsa(&status[0]); // 更改这里的参数来切换测试用例
+  Result const *result = majsa(&status[0]);  // 更改这里的参数来切换测试用例
   if (result != NULL)
     outputResultJson(*result);
   // OJ 会对比输出结果和标准结果，按照对比结果给出评分
@@ -50,9 +50,10 @@ void outputResultJson(Result result) {
   fprintf(f, "{\"Type\":%d,\"Yaku\":[", result.type);
   for (int i = 0; i < 20; i++) {
     fprintf(f, "%d", result.yaku[i]);
-    if (i < 19) {
+    if (i < 19 && result.yaku[i + 1] != 0)
       fprintf(f, ",");
-    }
+    else if (i < 19 && result.yaku[i + 1] == 0)
+      break;
   }
   fprintf(f, "],\"Han\":%d,\"Fu\":%d,\"Point\":[%d,%d,%d],\"Machi\":%d,\"Shanten\":%d}", result.han, result.fu, result.point[0], result.point[1], result.point[2], result.machi, result.shanten);
   fclose(f);
