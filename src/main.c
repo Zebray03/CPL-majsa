e63185ce0b84d90de7747e32c56bc53785a164ed
Sakiyary <sakiyary19@qq.com>
2024-01-21 23:44:17 +0800

    新增样例、编译脚本，纠错

--- src/main.c
+++ src/main.c
@@ -2,24 +2,43 @@
 #include "majsa.h"
 
 int main() {
-  Status status = {
-      .bakaze = TON,
-      .jikaze = TON,
-      .honbaCount = 0,
-      .dora = "1m",
-      .uradora = "3s",
-      .handTile = "1m9m1p9p1s9s1z2z3z4z5z6z7z",
-      .groupTile = {},
-      .discardTile = "2m3m4m5m3p4p5p6p2s3s4s5s",
-      .currentPlayer = JICHA,
-      .currentTile = "1z",
-      .remainTileCount = 18,
-      .isRiichi = false,
-      .isDoubleRiichi = false,
-      .isIppatsu = false,
-      .isRinshan = false,
+  Status status[] = {
+      {
+          .bakaze = TON,
+          .jikaze = TON,
+          .honbaCount = 0,
+          .dora = "1m",
+          .uradora = "3s",
+          .handTile = "1m9m1p9p1s9s1z2z3z4z5z6z7z",
+          .groupTile = {},
+          .discardTile = "2m3m4m5m3p4p5p6p2s3s4s5s",
+          .currentPlayer = JICHA,
+          .currentTile = "1z",
+          .remainTileCount = 18,
+          .isRiichi = false,
+          .isDoubleRiichi = false,
+          .isIppatsu = false,
+          .isRinshan = false,
+      },
+      {
+          .bakaze = TON,
+          .jikaze = NANN,
+          .honbaCount = 1,
+          .dora = "9p",
+          .uradora = "5m",
+          .handTile = "4m5m6m7m7m8m8m5s6s6s7s7s8s",
+          .groupTile = {},
+          .discardTile = "3z5z1z2m3p2m3s7s",
+          .currentPlayer = KAMICHA,
+          .currentTile = "8m",
+          .remainTileCount = 37,
+          .isRiichi = true,
+          .isDoubleRiichi = false,
+          .isIppatsu = true,
+          .isRinshan = false,
+      },
   };
-  Result const *result = majsa(&status);
+  Result const *result = majsa(&status[0]);
   if (result != NULL)
     outputResultJson(*result);
   // OJ 会对比输出结果和标准结果，按照对比结果给出评分
@@ -28,13 +47,13 @@ int main() {
 
 void outputResultJson(Result result) {
   FILE *f = fopen("result.json", "w");
-  fprintf(f, "{\"type\":%d,\"yaku\":[", result.type);
+  fprintf(f, "{\"Type\":%d,\"Yaku\":[", result.type);
   for (int i = 0; i < 20; i++) {
     fprintf(f, "%d", result.yaku[i]);
     if (i < 19) {
       fprintf(f, ",");
     }
   }
-  fprintf(f, "],\"han\":%d,\"fu\":%d,\"point\":[%d,%d,%d],\"machi\":%d,\"shanten\":%d}", result.han, result.fu, result.point[0], result.point[1], result.point[2], result.machi, result.shanten);
+  fprintf(f, "],\"Han\":%d,\"Fu\":%d,\"Point\":[%d,%d,%d],\"Machi\":%d,\"Shanten\":%d}", result.han, result.fu, result.point[0], result.point[1], result.point[2], result.machi, result.shanten);
   fclose(f);
 }
--- src/majsa.c
+++ src/majsa.c
@@ -2,15 +2,29 @@
 
 Result *majsa(Status *status) {
   Result *result = (Result *)malloc(sizeof(Result));
-  Result example = {
+  Result example0 = {
       .type = TSUMO,
       .yaku = {Kokushijuusanmenmachi},
-      .han = 26,
-      .fu = 30,
+      .han = -2,
+      .fu = -1, // -1 代表 OJ 不会测评该条目
       .point = {32000, 32000, 32000},
-      .machi = 13,
-      .shanten = 0,
+      .machi = -1, // -1 代表 OJ 不会测评该条目
+      .shanten = -1, // -1 代表 OJ 不会测评该条目
   };
-  memcpy(result, &example, sizeof(Result));
+  Result example1 = {
+      .type = RON,
+      .yaku = {Riichi, Ippatsu, Tanyao},
+      .han = 5,
+      .fu = 40,
+      .point = {8300, 0, 0},
+      .machi = -1, // -1 代表 OJ 不会测评该条目
+      .shanten = -1, // -1 代表 OJ 不会测评该条目
+  };
+  if (status->currentPlayer == JICHA) {
+    memcpy(result, &example0, sizeof(Result));
+  }
+  if (status->currentPlayer == KAMICHA) {
+    memcpy(result, &example1, sizeof(Result));
+  }
   return result;
 }
