4719f3e468417bcd056bd50d752090b5276a2afb
Sakiyary <sakiyary19@qq.com>
2024-12-11 04:16:50 +0800

    update compile scripts and public answer and Makefile

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@
 all: majsa
 
 majsa: git
-	gcc -g -O2 $(shell find ./src -name *.c) -I. -Wall -Wextra -o majsa
+	gcc -g -O2 $(shell find ./src -name *.c) -std=c17 -lm -I. -Wall -Wextra -o majsa
 
 run: majsa
 	./majsa
--- compile.ps1
+++ compile.ps1
@@ -1,2 +1,4 @@
 # 使用 Windows 下的 VS Code 的同学可以在 PowerShell 终端中运行此脚本来编译 majsa
-gcc $(Get-ChildItem -Recurse -Filter "*.c" | ForEach-Object { $_.FullName }) -std=c17 -lm -march=native -O2 -fno-strict-aliasing -Wall -DONLINE_JUDGE -fdiagnostics-color=always -o majsa
+git add -A
+git commit --allow-empty -m "compile"
+gcc $(Get-ChildItem -Recurse "./src" -Filter "*.c" | ForEach-Object { $_.FullName }) -std=c17 -lm -march=native -O2 -fno-strict-aliasing -Wall -DONLINE_JUDGE -fdiagnostics-color=always -o majsa
